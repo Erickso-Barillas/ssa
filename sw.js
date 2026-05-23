d459a95ec607b1d83a00a1b25b78de9bf2898c4e
Erickso-Barillas <141478209+Erickso-Barillas@users.noreply.github.com>
2026-05-23 17:31:00 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'scsa-v2';
+const CACHE_NAME = 'scsa-v3';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -7,40 +7,48 @@ const ASSETS_TO_CACHE = [
   './Icono/android-icon-192x192.png',
   './Icono/ms-icon-310x310.png',
   './Icono/apple-icon-180x180.png',
-  './Fotos/Camiones de suministros/s1.jpg',
-  './Fotos/Camiones de suministros/s2.jpg',
-  './Fotos/Camiones de suministros/s3.jpg',
-  './Fotos/Camiones de suministros/s4.jpg',
-  './Fotos/Camiones de suministros/s5.jpg',
-  './Fotos/Camiones de suministros/s6.jpg',
-  './Fotos/Tipo panel/pa1.jpg',
-  './Fotos/Tipo panel/pa2.jpg',
-  './Fotos/Tipo panel/pa3.jpg',
-  './Fotos/Tipo panel/pa4.jpg',
-  './Fotos/Tipo panel/pa5.jpg',
-  './Fotos/Pick up/p1.jpg',
-  './Fotos/Pick up/p2.jpg',
-  './Fotos/Pick up/p3.jpg',
-  './Fotos/Pick up/p4.jpg',
-  './Fotos/Pick up/p5.jpg',
-  './Fotos/Pick up/p6.jpg',
-  './Fotos/Pick up/p7.jpg',
-  './Fotos/Tracto remolque/t1.jpg',
-  './Fotos/Tracto remolque/t2.jpg',
-  './Fotos/Tracto remolque/t3.jpg',
-  './Fotos/Tracto remolque/t4.jpg',
-  './Fotos/Tracto remolque/t5.jpg',
-  './Fotos/Tracto remolque/t6.jpg'
+  './Fotos/Camiones%20de%20suministros/s1.png',
+  './Fotos/Camiones%20de%20suministros/s2.png',
+  './Fotos/Camiones%20de%20suministros/s3.png',
+  './Fotos/Camiones%20de%20suministros/s4.png',
+  './Fotos/Camiones%20de%20suministros/s5.png',
+  './Fotos/Camiones%20de%20suministros/s6.png',
+  './Fotos/Tipo%20panel/pa1.png',
+  './Fotos/Tipo%20panel/pa2.png',
+  './Fotos/Tipo%20panel/pa3.png',
+  './Fotos/Tipo%20panel/pa4.png',
+  './Fotos/Tipo%20panel/pa5.png',
+  './Fotos/Pick%20up/p1.png',
+  './Fotos/Pick%20up/p2.png',
+  './Fotos/Pick%20up/p3.png',
+  './Fotos/Pick%20up/p4.png',
+  './Fotos/Pick%20up/p5.png',
+  './Fotos/Pick%20up/p6.png',
+  './Fotos/Pick%20up/p7.png',
+  './Fotos/Tracto%20remolque/t1.png',
+  './Fotos/Tracto%20remolque/t2.png',
+  './Fotos/Tracto%20remolque/t3.png',
+  './Fotos/Tracto%20remolque/t4.png',
+  './Fotos/Tracto%20remolque/t5.png',
+  './Fotos/Tracto%20remolque/t6.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))));
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
 });
